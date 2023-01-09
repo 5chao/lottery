40607c4f976842236c244e7f629b33f611218639
李武朝 <liwc@udiannet.com>
2023-01-09 20:24:21 +0800

    perf:信息

---
+++ .history/pages/index_20230109085102.js
@@ -0,0 +1,50 @@
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Content from "../components/Content";
+import Comments from "../components/Comments";
+
+const Home = ({ comments = [] }) => {
+  return (
+    <div>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Header />
+        <Grid container spacing={5} sx={{ mt: 3 }}>
+          <Grid
+            item
+            xs={12}
+            sx={{
+              py: 3,
+            }}
+          >
+            <Content />
+            <Divider
+              variant="middle"
+              sx={{
+                my: 3,
+              }}
+            />
+            <Comments initialComments={comments} />
+          </Grid>
+        </Grid>
+        <Footer />
+      </Container>
+    </div>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_HOSTNAME}/api/comment`);
+  const comments = await res.json();
+
+  return {
+    props: { comments },
+  };
+};
+
+export default Home;
